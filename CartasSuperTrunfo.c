3771c88441e42498e2add83055ae75326cdb8c1c
RafaelP93 <rafaelufc93@gmail.com>
2025-02-21 11:20:16 +0000

    codigo do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int população, pontosturisticos;
+    int populacao, pontosturisticos;
     char estado[50], codigo[5], cidade[50];
     float area, PIB;
 
@@ -11,16 +11,16 @@ int main() {
     printf("Digite o nome da cidade: \n");
     scanf("%s", &cidade);
 
-    printf("Código: \n");
+    printf("Código da Carta: \n");
     scanf("%s", &codigo);
 
-    printf("Área em (km²): \n");
+    printf("Área em km²: \n");
     scanf("%f", &area);
 
     printf("População: \n");
-    scanf("%d", &população);
+    scanf("%d", &populacao);
 
-    printf("Qual p PIB: \n");
+    printf("Qual o PIB: \n");
     scanf("%f", &PIB);
 
     printf("Quantos pontos turisticos: \n");
@@ -30,9 +30,10 @@ int main() {
     printf("Código: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
     printf("Área em km²: %f\n", area);
-    printf("População: %d\n", população);
+    printf("População: %d\n", populacao);
     printf("PIB: %f\n", PIB);
-    printf("Pontos Turisticos: %d", pontosturisticos);
+    printf("Pontos turisticos: %d\n", pontosturisticos);
 
     return 0;
+
 }
